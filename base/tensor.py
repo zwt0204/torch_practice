960459b1f583da3e9ed2375793bdb457bc4997e6
weitao.zhang <weitao.zhang@weimob.com>
2020-10-26 17:28:21 +0800

    example

--- base/tensor.py
+++ base/tensor.py
@@ -103,9 +103,52 @@ print(z, out)
 
 a = torch.randn(2, 2)
 a = ((a * 3) / (a - 1))
-print(a.requires_grad)
+# print(a.requires_grad)
 a.requires_grad_(True)
-print(a.requires_grad)
+# print(a.requires_grad)
 b = (a * a).sum()
-print(b.grad_fn)
+# print(b.grad_fn)
+
+print('=================')
+out.backward()
+print(x.grad)
+
+x = torch.randn(3, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+print(y)
+
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+
+print(x.grad)
+
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+    print((x ** 2).requires_grad)
+
+
+print(x.requires_grad)
+y = x.detach()
+print(y.requires_grad)
+print(x.eq(y).all())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
